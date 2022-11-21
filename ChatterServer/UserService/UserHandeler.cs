c2a634dae19c4da248280b3660b1769251f95ac7
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2022-11-21 19:19:39 +0100

    Added saving and loading

--- ChatterServer/UserService/UserHandeler.cs
+++ ChatterServer/UserService/UserHandeler.cs
@@ -1,32 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Chatter.Server.UserService
 {
-    public class UserHandeler
-    {
-        public static List<User> users = new List<User>();
-        public UserHandeler() 
-        {
-            users = new List<User>();
-            new TokenHandeler();
-        }
-        public static User GetUser(int id)
-        {
-            return users[id];
-        }
-        public static void AddUser(User user)
-        {
-            users.Add(user);
-        }
-        public static int GetNewId() 
-        {
-            int id = 0;
-            users.ForEach(user => { if (user.Id >= id) { id = user.Id + 1; } });
-            return id;
-        }
-    }
+	public class UserHandeler
+	{
+		public static List<User> users;
+		public static string Path;
+		public UserHandeler(string path) 
+		{
+			users = new List<User>();
+			new TokenHandeler();
+			Path = path;
+		}
+
+		public static void Load()
+		{
+			if (!File.Exists(Path))
+			{
+				return;
+			}
+			users = (List<User>)JsonConvert.DeserializeObject(File.ReadAllText(Path));
+		}
+		public static void Save() 
+		{
+			File.WriteAllText(Path,JsonConvert.SerializeObject(users));
+		}
+		public static User GetUser(int id)
+		{
+			foreach (User user in users)
+			{
+				if (user.Id == id)
+				{
+					return user;
+				}
+			}
+			return null;
+		}
+		public static void AddUser(User user)
+		{
+			users.Add(user);
+		}
+		public static int GetNewId() 
+		{
+			int id = 0;
+			users.ForEach(user => { if (user.Id >= id) { id = user.Id + 1; } });
+			return id;
+		}
+	}
 }
