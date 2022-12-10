c5f63055dcb912c465ba175171b061a6808329f1
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2022-12-10 12:17:05 +0100

    client almost works

--- ChatterClient/Program.cs
+++ ChatterClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleTCP;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,12 +9,14 @@ namespace ChatterClient
 {
     internal static class Program
     {
+        static SimpleTcpClient _Client;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            _Client = new SimpleTcpClient();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
